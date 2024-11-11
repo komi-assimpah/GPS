1acef0358a430e0da2473af5878e6dd3debf73d7
komi-assimpah <komi-jean-paul.assimpah@etu.unice.fr>
2024-11-11 22:05:02 +0100

    css added

--- components/search-bar/search-bar.js
+++ components/search-bar/search-bar.js
@@ -21,6 +21,7 @@ class SearchBar extends HTMLElement {
                 }
                 input:focus {
                     border-color: #CE472F;
+                    border-width: 2px;
                     box-shadow: 0 0 5px rgba(206, 71, 47, 0.5); 
                     }
                 .suggestions {
